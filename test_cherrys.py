d90922563a430b32e91a897dd86539c87643fc1f
Ralph Heinkel <rh@ralph-heinkel.com>
2023-02-16 14:06:41 +0100

    Make cherrys work with modern redis and cherrypy version.
    Upgraded to Python 3 (only)
    Support distributed session locking via redis locks
    Switched to pytest testing (cause cherrypy does the same)
    Support redis-url for connecting to redis server
    Setup sensible default values for optional connection parameters.
    Some code reformatting to fit pep8/flake8 styling requirements

--- test_cherrys.py
+++ test_cherrys.py
@@ -1,16 +1,16 @@
-import unittest
 import socket
+import time
+import threading
 
+import cherrys
 import cherrypy
-from cherrypy.test import webtest
+import pytest
+from cherrypy.test import helper
 
-# testing that redis-py is available and that we have a redis server running
-try:
-    import redis
 
-    host, port = '127.0.0.1', 6379
-    for res in socket.getaddrinfo(host, port, socket.AF_UNSPEC,
-                                  socket.SOCK_STREAM):
+def setup_module():
+    host, port = '127.0.0.1', cherrys.REDIS_PORT
+    for res in socket.getaddrinfo(host, port, socket.AF_UNSPEC, socket.SOCK_STREAM):
         af, socktype, proto, canonname, sa = res
         s = None
         try:
@@ -21,86 +21,118 @@ try:
         except socket.error:
             if s:
                 s.close()
-            raise
+            pytest.exit(f"Failed to connect to redis server", returncode=2)
         break
 
-except ImportError:
 
-  class RedisSessionTest(unittest.TestCase):
-      def test_nothing(self):
-          self.fail("redis-py not available")
+class RedisSessionTest(helper.CPWebCase):
+    """RedisSession unittest class - connect to redis via hostname/port"""
+    helper.CPWebCase.interactive = False
 
-except socket.error:
+    @staticmethod
+    def setup_server():
+        cherrypy.config.update({
+            'log.screen': False,  # set to True to see error stdout and errors from cp server
+            'tools.sessions.on': True,
+            'tools.session.host': "localhost",
+            'tools.session.port': cherrys.REDIS_PORT,
+            'tools.session.url': None,
+            'tools.sessions.storage_class': cherrys.RedisSession
+        })
+        cherrypy.tree.mount(App())
 
-  class RedisSessionTest(unittest.TestCase):
-      def test_nothing(self):
-          self.fail("redis not reachable")
+    def test_server_working(self):
+        self.getPage('/')
+        self.assertStatus(200)
 
-# all good to go we can test
-else:
+    def test_deleting_non_existing_key_fails(self):
+        self.getPage('/delete')
+        self.assertStatus(500)
 
-    import cherrys
-    cherrypy.lib.sessions.RedisSession = cherrys.RedisSession
+    def test_deleting_stored_data(self):
+        self.getPage('/store')
+        self.assertStatus(200)
+        # first getPage call sets a cookie
+        # second getPage call needs to be aware of the cookie
+        self.getPage('/delete', self.cookies)
+        self.assertStatus(200)
 
-    class RedisSessionTest(webtest.WebCase):
+    def test_storing_data(self):
+        self.getPage('/store?sleep=5')
+        self.assertStatus(200)
+        self.assertBody('redis')
 
-        interactive = False
+    def test_retrieving_stored_data(self):
+        self.getPage('/retrieve')
+        self.assertStatus(500)
+        self.getPage('/store', self.cookies)
+        self.assertStatus(200)
+        self.assertBody('redis')
+        self.getPage('/retrieve', self.cookies)
+        self.assertStatus(200)
 
-        @classmethod
-        def setUpClass(cls):
-            # configuring CP instead of webCase (getPage - openUrl on port 8000)
-            cherrypy.config.update({
-                'server.socket_port' : 8000,
-                'log.screen' : False,
-                'tools.sessions.on' : True,
-                'tools.sessions.storage_type' : 'redis'
-            })
-            cherrypy.tree.mount(App())
-            cherrypy.engine.start()
+    def test_locked_session(self):
+        """Check that redis really holds a lock on a session.
 
-        def test_server_working(self):
-            self.getPage('/')
-            self.assertStatus(200)
+        This is achieved by sending two parallel requests with the same session id to
+        the cherrypy server. The first request makes the session block for SLEEP_SECS.
+        The second request should be forced to wait at least SLEEP_SECS before it
+        can be accomplished.
+        """
+        SLEEP_SECS = 1.0
+        self.getPage('/')
+        # Cookies must be stored locally to be usable in background thread:
+        cookies = self.cookies[:]
+        thread = threading.Thread(
+            target=self.getPage, args=[f'/store?sleep={SLEEP_SECS}', cookies]
+        )
+        thread.start()
 
-        def test_deleting_non_existing_key_fails(self):
-            self.getPage('/delete')
-            self.assertStatus(500)
+        t0 = time.time()
+        # Give background thread a chance to fire out the first request:
+        time.sleep(SLEEP_SECS / 4)
 
-        def test_deleting_stored_data(self):
-            self.getPage('/store')
-            self.assertStatus(200)
-            # first getPage call sets a cookie
-            # second getPage call needs to be aware of the cookie
-            self.getPage('/delete', self.cookies)
-            self.assertStatus(200)
+        # Then launch the 2nd request with the same session id:
+        self.getPage('/retrieve', cookies)
+        # This check ensures that the 2nd request indeed had to wait until the first
+        # request of the background thread (which blocked the session on the server
+        # side) actually has finished:
+        assert (time.time() - t0) >= SLEEP_SECS
 
-        def test_storing_data(self):
-            self.getPage('/store')
-            self.assertStatus(200)
-            self.assertBody('redis')
+        self.assertStatus(200)
+        self.assertBody('redis')
 
-        def test_retrieving_stored_data(self):
-            self.getPage('/retrieve')
-            self.assertStatus(500)
-            self.getPage('/store', self.cookies)
-            self.assertStatus(200)
-            self.assertBody('redis')
-            self.getPage('/retrieve', self.cookies)
-            self.assertStatus(200)
 
-        @classmethod
-        def tearDownClass(cls):
-            cherrypy.engine.exit()
+class RedisSessionTestViaUrl(RedisSessionTest):
+    """RedisSession unittest class - connect via redis-URL"""
 
-class App(object):
+    @staticmethod
+    def setup_server():
+        cherrypy.config.update({
+            'log.screen': False,  # set to True to see error stdout and errors from cp server
+            'tools.sessions.on': True,
+            'tools.session.host': None,
+            'tools.session.port': None,
+            'tools.session.url': "redis://localhost:6379/0",
+            'tools.sessions.storage_class': cherrys.RedisSession
+        })
+        cherrypy.tree.mount(App())
+
+
+class App:
     """ A basic application to test sessions """
+    app_data = None
 
     @cherrypy.expose
     def index(self):
+        assert isinstance(cherrypy.serving.session, cherrys.RedisSession)
+        cherrypy.session.load()
         return 'index'
 
     @cherrypy.expose
-    def store(self):
+    def store(self, sleep=None):
+        if sleep:
+            time.sleep(float(sleep))
         cherrypy.session['data'] = 'redis'
         return cherrypy.session['data']
 
@@ -112,6 +144,3 @@ class App(object):
     @cherrypy.expose
     def retrieve(self):
         return cherrypy.session['data']
-
-if __name__ == '__main__':
-  unittest.main()
